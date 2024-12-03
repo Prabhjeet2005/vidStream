748018e9a82aacaba9b5de60134c0717a752f884
Prabhjeet <prabhjeetsandhu010@gmail.com>
2024-12-03 22:03:56 +0530

    6:13:28 complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import express from "express";
-{
-}
+import dotenv from "dotenv";
+
+import connectDb from "./db/db.js";
+import { app } from "./app.js";
+dotenv.config();
+
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Listening on PORT: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Connection MongoDB Failed: ${error}`);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+//Async/Await
+
+const asyncHandler = (requestHandler) => {
+  async (req, res, next) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (error) {
+      res.status(error.code || 500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+};
+
+//Promise
+// const asyncHandler = (requestHandler) => {
+//     (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch ((err) => {
+//             next(err);
+//         })
+//     }
+// }
+
+export {asyncHandler}
