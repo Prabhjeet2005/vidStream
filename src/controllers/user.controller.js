87d72f7386ac846a2efaf83ce4c15d84c9b042eb
Prabhjeet <prabhjeetsandhu010@gmail.com>
2024-12-22 23:31:46 +0530

    Bug: findByIdAndUpdate instead of findByIdAndDelete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -196,7 +196,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 //  Before Logout Create auth.middleware.js
 const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndDelete(
+  await User.findByIdAndUpdate(
     req.user._id,
     {
       $set: {
